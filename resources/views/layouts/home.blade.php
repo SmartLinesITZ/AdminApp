94a2da7ddde80749137937ee90ffceac5ebd0350
Alex-GT <isc.agt@gmail.com>
2017-04-18 22:33:51 -0500

    Laravel AdminApp

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<title>{{ config('app.name', 'Laravel') }}</title>
+
+<!-- Styles -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/css/material-fullpalette.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/css/ripples.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+<link rel="stylesheet" type="text/css" href="{{ url('/css/app.css') }}">
+<!-- Scripts -->
+<script>
+    window.Laravel = <?php echo json_encode([
+        'csrfToken' => csrf_token(),
+    ]); ?>
+</script>
+</head>
+<body>
+
+    @yield('content')
+
+
+<!-- Scripts -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+ <script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/js/material.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/js/ripples.min.js"></script>
+ <script >$.material.init();</script>
+<script src="/js/app.js"></script>
+</body>
+</html>
---
+++ resources/views/resturantes/index.blade.php
@@ -0,0 +1,40 @@
+
+@extends("layouts.home");
+@section("content")
+<div class="big-padding text-center blue-grey whitw-text">
+	<h1>Prouctos</h1>
+</div>
+<div class="container">
+	<table class="table table-bordered">
+		<thead>
+		<tr>
+			<td>Nombre de restaurante</td>
+			<td>Admin</td>
+			<td>Correo</td>
+			<td>Telefono</td>
+			<td>Acciones</td>
+		</tr>
+		</thead>
+		<tbody>
+			@foreach ($restaurantes as $restaurante)
+			<tr>
+				<td>{{ $restaurante->nombrerest }}</td>
+				<td>Nombre del Adminitrador</td>
+				<td>Correo</td>
+				<td>{{ $restaurante->telefono }}</td>
+				<td>
+					<!--a href="{{url('/products/'.$product->id" -->
+					Editar
+					</a>
+				</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+</div>
+<div class="floating">
+	<!--a href="{{url('/products/create')}}" class="btn btn-primary btn-fab">
+		<i class="material-icons">add</i >
+	</a-->
+</div>
+@endsection
